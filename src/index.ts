4f52f1ccaae7b140574bf7a38bc21c0d953c4a1f
fatiudeen <shehufatiudeen@gmail.com>
2022-07-08 09:08:01 +0100

    added ssl

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { createServer } from 'http2';
+import { createSecureServer } from 'http2';
 import app from './api/app';
-// import { readFileSync } from 'fs'
+import { readFileSync } from 'fs';
 
-// const options = {
-//   key: readFileSync('<Certificate Key>'),
-//   cert: readFileSync('<Certificate file>'),
-//   allowHTTP1: true,
-// };
-const server = createServer(app.server());
+const options = {
+  key: readFileSync('localhost-privkey.pem'),
+  cert: readFileSync('localhost-cert.pem'),
+  allowHTTP1: true,
+};
+const server = createSecureServer(options, app.server());
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log('connected');
